5ac3f30b34ec9fa2319d83a9604a4f52910dc257
Aliaksandr Zhur <sanchesmrio1@gmail.com>
2019-10-13 00:11:39 +0300

    solve 'brackets task'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let length = arr.length;
+  while (length > 1){
+      for (let i = 0; i < arr.length; i++){
+          for (let j = 0; j < bracketsConfig.length; j++){
+              if (arr[i] === bracketsConfig[j][0] && (arr[i + 1] === bracketsConfig[j][1])){
+                  arr.splice(i,2);
+              }
+          }
+      }
+      length -= 2;
+   }
+   if (arr.length == 0){
+       return true
+   } else {
+       return false
+   }               
 }
